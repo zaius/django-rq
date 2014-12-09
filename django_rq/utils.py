506e8acdd9290f9ea6141139ed387b40c93b20f6
David Kelso <david@kelso.id.au>
2014-12-08 17:45:28 -0800

    Add workers and scheduled jobs to stats page.

--- django_rq/utils.py
+++ django_rq/utils.py
@@ -6,10 +6,13 @@ from rq.worker_registration import clean_worker_registry
 from .queues import get_connection, get_queue_by_index
 from .settings import QUEUES_LIST
 from .templatetags.django_rq import to_localtime
+from .workers import get_worker_class
 
 
 def get_statistics(run_maintenance_tasks=False):
     queues = []
+    workers = []
+    worker_class = get_worker_class()
     for index, config in enumerate(QUEUES_LIST):
 
         queue = get_queue_by_index(index)
@@ -56,5 +59,51 @@ def get_statistics(run_maintenance_tasks=False):
         queue_data['deferred_jobs'] = len(deferred_job_registry)
         queue_data['failed_jobs'] = len(failed_job_registry)
 
+        workers += worker_class.all(connection=connection)
+
         queues.append(queue_data)
-    return {'queues': queues}
+
+    # TODO: Right now the scheduler can run on multiple queues, but multiple
+    # queues can use the same connection. Either need to dedupe connections or
+    # split scheduled into its own queue, like failed.
+    #
+    # TODO: the real solution here is ditch allowing queues to have separate
+    # connections - make a single global connection and multiple queues are
+    # only separated by name. This will solve the multiple failed queue issue
+    # too. But was there a reason to allow multiple connections? Also, this
+    # will require some massive doc updates.
+    scheduled_jobs = []
+    scheduler_running = False
+    scheduler_installed = False
+    try:
+        from rq_scheduler import Scheduler
+        scheduler_installed = True
+    except ImportError:
+        pass
+    else:
+        connection = get_connection('default')
+        scheduler = Scheduler(connection=connection)
+        # get_jobs with_times returns a list of tuples: (job, datetime)
+
+        # TODO: job.origin is the scheduler queue originally used to schedule
+        # the job. Need to check if this is how the scheduler actually picks
+        # which queue to put the job into.
+        for job in scheduler.get_jobs(with_times=True):
+            scheduled_jobs.append({
+                'job': job[0],
+                'runtime': job[1],
+                'queue': job[0].origin,
+            })
+
+        # TODO: should expose this from rq-scheduler.
+        # TODO: this is really per-queue.
+        scheduler_running = connection.exists(scheduler.scheduler_key) and \
+            not connection.hexists(scheduler.scheduler_key, 'death')
+
+    return {
+        'queues': queues,
+        'workers': list(set(workers)),
+        'scheduler_installed': scheduler_installed,
+        'scheduler_running': 'running' if scheduler_running else 'stopped',
+        'scheduled_jobs': scheduled_jobs,
+    }
